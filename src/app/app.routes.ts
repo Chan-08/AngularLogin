135b86062f74f3c9fcc3249f0df6c20369a613c5
Chandrakumar <itz.chandrakumar@gmail.com>
2025-02-04 23:33:09 +0530

    first commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
+  { path: 'dashboard', loadChildren: () => import('./pages/dashboard/dashboard.module').then(m => m.DashboardModule) },
+  { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) }
+];
---
+++ src/app/login/login/login.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+  email = '';
+  password = '';
+  errorMessage = '';
+
+  constructor(private router: Router, private http: HttpClient) {}
+
+  login() {
+    this.http.get<any[]>('/assets/users.json').subscribe(users => {
+      const user = users.find(u => u.email === this.email && u.password === this.password);
+      if (user) {
+        this.router.navigate(['/dashboard']);
+      } else {
+        this.errorMessage = 'Invalid email or password!';
+      }
+    });
+  }
+}
